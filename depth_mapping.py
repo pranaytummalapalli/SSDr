8521d97e6fce3bd224e67576c6bc44cc1cdb88b6
Kshitij Sidana <30952448+KshitijSidana@users.noreply.github.com>
2018-03-01 20:25:53 +0530

    Update depth_mapping.py
    Added modified code of stereo_match.py under python sample
    added video inputs and down scaled them
    changed some parameters and removed the saving part from the original file

--- depth_mapping.py
+++ depth_mapping.py
@@ -1,1 +1,41 @@
+#!/usr/bin/env python
 
+import numpy as np
+import cv2 as cv
+
+cap1 = cv.VideoCapture(0)
+cap2 = cv.VideoCapture(1)
+
+
+if __name__ == '__main__':
+    while True :
+        ret1, imgR = cap1.read()
+        cv.waitKey(1)
+        ret2, imgL = cap2.read()
+
+        imgR = cv.resize(imgR, (0, 0), fx=0.5, fy=0.5)
+        imgL = cv.resize(imgL, (0,0), fx=0.5, fy=0.5)
+        #imgL = cv.pyrDown( cv.imread('/home/kshitij/opencv-3.4.0/samples/data/aloeL.jpg') )  # downscale images for faster processing
+        #imgR = cv.pyrDown( cv.imread('/home/kshitij/opencv-3.4.0/samples/data/aloeR.jpg') )
+        #imgL = cv.pyrDown(img1)
+        #imgR = cv.pyrDown(img2)
+        window_size = 3
+        min_disp = 16
+        num_disp = 112-min_disp
+        stereo = cv.StereoSGBM_create(minDisparity = min_disp,
+            numDisparities = num_disp,
+            blockSize = 1,
+            P1 = 1*3*window_size**2,
+            P2 = 5*3*window_size**2,
+            disp12MaxDiff = 1,
+            uniquenessRatio = 10,
+            speckleWindowSize = 100,
+            speckleRange = 32
+        )
+        disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+        cv.imshow('left', imgL)
+        cv.imshow('right', imgR)
+        cv.imshow('disparity', (disp-min_disp)/num_disp)
+
+    cv.waitKey()
+    cv.destroyAllWindows()
