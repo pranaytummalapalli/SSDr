4aaad91a6b97678f50f149d93560c3d80f0d238a
Kshitij Sidana <30952448+KshitijSidana@users.noreply.github.com>
2018-03-03 01:04:02 +0530

    Code added. some changes needed
    added code for object detection 
    the location for the cascade has to be changed along with the camera number

--- object_detection.py
+++ object_detection.py
@@ -1,1 +1,16 @@
+import cv2
 
+hand_cascade = cv2.CascadeClassifier('/home/kshitij/PycharmProjects/Hand_Rec/haarcascade_frontalcatface.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+
+    #img = cv2.resize(img, (320, 240), interpolation=cv2.INTER_LINEAR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hands = hand_cascade.detectMultiScale(gray,3,5)
+    for (x, y, w, h) in hands:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('10', img)
+    cv2.waitKey(10)
