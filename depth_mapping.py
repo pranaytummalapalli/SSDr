7e2d00e1d11d11e6423eb59a00704c6f7c5f5bd5
Kshitij Sidana <30952448+KshitijSidana@users.noreply.github.com>
2018-03-03 16:01:41 +0530

    Update depth_mapping.py
    minor changes and comments added

--- depth_mapping.py
+++ depth_mapping.py
@@ -10,7 +10,8 @@ cap2 = cv.VideoCapture(1)
 if __name__ == '__main__':
     while True :
         _, imgR = cap1.read()
-        cv.waitKey(1)
+        # add the delay in case of an error or, if only one camera is being read
+        # cv.waitKey(1)
         _, imgL = cap2.read()
 
         imgR = cv.resize(imgR, (0,0), fx=0.5, fy=0.5)
@@ -19,7 +20,8 @@ if __name__ == '__main__':
         window_size = 3
         min_disp = 16
         num_disp = 112-min_disp
-        stereo = cv.StereoSGBM_create(minDisparity = min_disp,
+        stereo = cv.StereoSGBM_create(
+            minDisparity = min_disp,
             numDisparities = num_disp,
             blockSize = 1,
             P1 = 1*3*window_size**2,
@@ -28,7 +30,7 @@ if __name__ == '__main__':
             uniquenessRatio = 10,
             speckleWindowSize = 100,
             speckleRange = 32
-        )
+        )# these para meters need to be tuned at the time of testing
         disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
         cv.imshow('left', imgL)
         cv.imshow('right', imgR)
