eb9fd3ed0eb7a13ede6e8475ada28176cdb50316
Kshitij Sidana <30952448+KshitijSidana@users.noreply.github.com>
2018-03-04 20:51:13 +0530

    Update and rename object_detection.py to human_detection.py
    added both human and face detection and some comments

--- human_detection.py
+++ human_detection.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, img = cap.read()
-
+    # human detection
     found, w = hog.detectMultiScale(img, winStride=(8, 8), padding=(32, 32), scale=1.05)
     found_filtered = []
     for ri, r in enumerate(found):
@@ -21,15 +21,13 @@ while True:
     draw_detections(img, found)
     draw_detections(img, found_filtered, 3)
     print('%d (%d) found' % (len(found_filtered), len(found)))
-    cv2.imshow('img', img)
-
+    #cv2.imshow('Human', img)
+    #resizing to make it faster
     #img = cv2.resize(img, (320, 240), interpolation=cv2.INTER_LINEAR)
-    '''
+    # face detection
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     hands = hand_cascade.detectMultiScale(gray,3,5)
     for (x, y, w, h) in hands:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-    cv2.imshow('10', img)
-    '''
-    
-    cv2.waitKey(1)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+    cv2.imshow('Human/Face', img)
+    cv2.waitKey(1) # delay given so that the camera feed works properly ... try if removal is possible
