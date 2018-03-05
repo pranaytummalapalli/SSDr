2762d2e6aad4858bf16b8ba00f6ae8bd009541c8
pranaytummalapalli <31589727+pranaytummalapalli@users.noreply.github.com>
2018-03-06 00:12:33 +0530

    Update rpiend.py

--- rpiend.py
+++ rpiend.py
@@ -3,6 +3,5 @@ ser=serial.serial('/dev/ttyACM0', 9600)
 #/dev/ttyAMC0 is an example of the port.
 while 1:
 	n=input('Input instruction: ')
-	string=str(n)
-	ser.write(string)
-	print string
+	ser.write(n)
+	print n
